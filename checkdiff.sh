a8bb50dd82fcb55d102f86f1bf02352201078303
Leo Belyi <leonid.belyi@mycase.com>
2022-02-27 13:57:34 -0800

    Change checkdiff to return status

--- checkdiff.sh
+++ checkdiff.sh
@@ -2,6 +2,12 @@ for file in .*;
 do
     if [ "$file" != ".git" ]; then
         echo $file
-        diff $file ../$file
+        diff -q $file ../$file
+        echo $status
+        if [[ "$status" -eq 1 ]]; then
+            exit 1
+        fi
     fi
 done
+
+exit 0
