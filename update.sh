2ca95647f5a78fc13d0b45ddb2b67fd1ed536cce
Leo Belyi <leonid.belyi@mycase.com>
2022-02-28 09:21:31 -0800

    Back up existing dotfiles into backup dir

--- update.sh
+++ update.sh
@@ -2,9 +2,11 @@ source checkdiff.sh
 
 if [[ "$?" == "1" ]]; then
     echo "Differences detected, updating local files!"
+    echo "putting backup files into '.dotfiles/backup/'"
     for file in .*;
     do
         if [ "$file" != ".git" ]; then
+            mkdir -p backup && cp ../$file backup/$file
             cp $file ../$file
         fi
     done
