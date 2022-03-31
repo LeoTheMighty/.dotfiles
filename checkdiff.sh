5fa4ce8a8536c95f3ce301e2d3b0f65cd898008c
Leo Belyi <leonid.belyi@mycase.com>
2022-03-31 16:58:53 -0700

    I forget what i did to this file
    You shouldn't really use it in any case no worries

--- checkdiff.sh
+++ checkdiff.sh
@@ -4,18 +4,16 @@ realpath () {
 cd "$(dirname "$(realpath "$0")")"; # cd into script directory
 
 exit_code=0
-
 for file in .*;
 do
     if [ "$file" != ".git" ] && [ "$file" != ".gitignore" ]; then
         diff -q "$file" "../$file"
         if [[ "$status" == "1" ]]; then
+            diff "$file" "../$file"
             exit_code=1
             break
         fi
     fi
 done
-echo $exit_code
-(exit $exit_code)
 
 cd -
